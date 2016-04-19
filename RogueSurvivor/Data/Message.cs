49543c05fca608dd9514ce8d94da0d52adc369f1
Kenneth Boyd <zaimoni@zaimoni.com>
2016-04-18 22:31:16 -0500

    make Data.Message use an automatically implemented property

--- RogueSurvivor/Data/Message.cs
+++ RogueSurvivor/Data/Message.cs
@@ -11,22 +11,11 @@ namespace djack.RogueSurvivor.Data
 {
   internal class Message
   {
-    private string m_Text;
+    public string Text { get; set; }
     private Color m_Color;
     private readonly int m_Turn;
 
-    public string Text
-    {
-      get
-      {
-        return this.m_Text;
-      }
-      set
-      {
-        this.m_Text = value;
-      }
-    }
-
+    
     public Color Color
     {
       get
@@ -49,11 +38,10 @@ namespace djack.RogueSurvivor.Data
 
     public Message(string text, int turn, Color color)
     {
-      if (text == null)
-        throw new ArgumentNullException("text");
-      this.m_Text = text;
-      this.m_Color = color;
-      this.m_Turn = turn;
+      if (text == null) throw new ArgumentNullException("text");
+      Text = text;
+      m_Color = color;
+      m_Turn = turn;
     }
 
     public Message(string text, int turn)
