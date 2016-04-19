6f5074a0c2edc48d603f0540141390afbe3b1baa
Kenneth Boyd <zaimoni@zaimoni.com>
2016-04-18 23:23:15 -0500

    switch Data.TileModel over to automatically implemented properties

--- RogueSurvivor/Data/TileModel.cs
+++ RogueSurvivor/Data/TileModel.cs
@@ -11,66 +11,20 @@ namespace djack.RogueSurvivor.Data
   internal class TileModel
   {
     public static readonly TileModel UNDEF = new TileModel("", Color.Pink, false, true);
-    private int m_ID;
-    private string m_ImageID;
-    private bool m_IsWalkable;
-    private bool m_IsTransparent;
-    private Color m_MinimapColor;
-
-    public int ID
-    {
-      get
-      {
-        return this.m_ID;
-      }
-      set
-      {
-        this.m_ID = value;
-      }
-    }
-
-    public string ImageID
-    {
-      get
-      {
-        return this.m_ImageID;
-      }
-    }
-
-    public bool IsWalkable
-    {
-      get
-      {
-        return this.m_IsWalkable;
-      }
-    }
-
-    public bool IsTransparent
-    {
-      get
-      {
-        return this.m_IsTransparent;
-      }
-    }
-
-    public Color MinimapColor
-    {
-      get
-      {
-        return this.m_MinimapColor;
-      }
-    }
-
+    public int ID { get; set; }
+    public string ImageID { get; private set; }
+    public bool IsWalkable { get; private set; }
+    public bool IsTransparent { get; private set; }
+    public Color MinimapColor { get; private set; }
     public bool IsWater { get; set; }
-
     public string WaterCoverImageID { get; set; }
 
-    public TileModel(string imageID, Color minimapColor, bool IsWalkable, bool IsTransparent)
+    public TileModel(string imageID, Color minimapColor, bool isWalkable, bool isTransparent)
     {
-      this.m_ImageID = imageID;
-      this.m_IsWalkable = IsWalkable;
-      this.m_IsTransparent = IsTransparent;
-      this.m_MinimapColor = minimapColor;
+      ImageID = imageID;
+      IsWalkable = isWalkable;
+      IsTransparent = isTransparent;
+      MinimapColor = minimapColor;
     }
   }
 }
