93f4206084015a38a8815a02ec889f7b19052e13
Kenneth Boyd <zaimoni@zaimoni.com>
2016-05-30 23:31:39 -0500

    start prototyping a denormalized probability distribution class (very unclear how to make police sweep districts)

---
+++ RogueSurvivor/Zaimoni/Data/DenormalizedProbability.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zaimoni.Data
+{
+    class DenormalizedProbability<T>
+    {
+        private Dictionary<T, int> _weights;
+
+        public DenormalizedProbability()
+        {
+            _weights = new Dictionary<T, int>();
+        }
+
+        public DenormalizedProbability(Dictionary<T, int> src)
+        {
+            _weights = new Dictionary<T, int>(src);
+        }
+
+        public DenormalizedProbability(DenormalizedProbability<T> src)
+        {
+            _weights = new Dictionary<T, int>(src._weights);
+        }
+
+        public void MakeSafeToScaleBy(int x)
+        {
+
+        }
+    }
+}
