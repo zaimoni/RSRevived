f17b348a76c4fee5114675f002c5cc11e94cd95a
Kenneth Boyd <zaimoni@zaimoni.com>
2016-06-01 02:16:30 -0500

    revise design for denormalized probability -- we can afford roundoff error, etc.

--- RogueSurvivor/Zaimoni/Data/DenormalizedProbability.cs
+++ RogueSurvivor/Zaimoni/Data/DenormalizedProbability.cs
@@ -4,26 +4,50 @@ using System.Linq;
 
 namespace Zaimoni.Data
 {
+    // Use float rather than double to conserve savefile size.
+    // Be sure to use industry-standard practice of double for intermediate calculations
+    // We plausibly won't get large enough for failure of int to fit in float to be a problem
+    // Note that we are still probability rather than quasiprobability; thus, negative values
+    // are illegal.
+    // Default value if no key is zero.
     class DenormalizedProbability<T>
     {
-        private Dictionary<T, int> _weights;
+        private Dictionary<T, float> _weights;
 
         public DenormalizedProbability()
         {
-            _weights = new Dictionary<T, int>();
+            _weights = new Dictionary<T, float>();
         }
 
-        public DenormalizedProbability(Dictionary<T, int> src)
+        public DenormalizedProbability(Dictionary<T, float> src)
         {
-            _weights = new Dictionary<T, int>(src);
+            _weights = new Dictionary<T, float>(src);
+            foreach(T tmp in new List<T>(_weights.Keys)) {
+                if (0 >= _weights[tmp]) _weights.Remove(tmp);
+            }
         }
 
         public DenormalizedProbability(DenormalizedProbability<T> src)
         {
-            _weights = new Dictionary<T, int>(src._weights);
+            _weights = new Dictionary<T, float>(src._weights);
         }
 
-        public void MakeSafeToScaleBy(int x)
+        public float this[T x]
+        {
+            get {
+                if (!_weights.ContainsKey(x)) return 0.0f;
+                return _weights[x];
+            }
+            set {
+                if (0 < value) {
+                    _weights[x] = value;
+                    return;
+                }
+                if (_weights.ContainsKey(x)) _weights.Remove(x);
+            }
+        }
+
+        public void Normalize(float target)
         {
 
         }
