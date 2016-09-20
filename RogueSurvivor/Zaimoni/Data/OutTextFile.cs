1658285b6dd9324da09bd0d9db0a31c9e8c8b2b8
Kenneth Boyd <zaimoni@zaimoni.com>
2016-09-19 19:10:21 -0500

    class to support a cheat option properly.

---
+++ RogueSurvivor/Zaimoni/Data/OutTextFile.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace Zaimoni.Data
+{
+    class OutTextFile
+    {
+        public readonly string filepath;
+        private StreamWriter _file = null;
+
+        OutTextFile(string dest) {
+            filepath = dest;
+        }
+
+        public void WriteLine(string src) {
+            lock (filepath) {
+                if (null == _file) {
+                    if (File.Exists(filepath)) File.Delete(filepath);
+                    _file = File.CreateText(filepath);
+                }
+                _file.WriteLine(src);
+                _file.Flush();
+            }
+        }
+
+        public void Close() {   // XXX C# goes inefficient with a true destructor so don't provide one
+            lock (filepath) {
+                if (null != _file) {
+                    _file.Close();
+                    _file = null;
+                }
+            }
+        }
+    }
+}
