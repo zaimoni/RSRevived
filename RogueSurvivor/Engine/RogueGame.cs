78d63aa5cc7c51cdb5ad657452c69394297e9c48
Kenneth Boyd <zaimoni@zaimoni.com>
2016-11-20 10:44:14 -0600

    bluffing being police also changes actor model

--- RogueSurvivor/Engine/RogueGame.cs
+++ RogueSurvivor/Engine/RogueGame.cs
@@ -10100,6 +10100,7 @@ namespace djack.RogueSurvivor.Engine
           upgradeActor.Doll.AddDecoration(DollPart.TORSO, GameImages.POLICE_UNIFORM);
           upgradeActor.Doll.AddDecoration(DollPart.LEGS, GameImages.POLICE_PANTS);
           upgradeActor.Doll.AddDecoration(DollPart.FEET, GameImages.POLICE_SHOES);
+          upgradeActor.Model = Models.Actors[(int)GameActors.IDs.POLICEMAN];
           upgradeActor.Controller = new PlayerController();
           AddMessage(new Data.Message("Welcome to the force.", Session.Get.WorldTime.TurnCounter, Color.Yellow));
         } else
