a58c96068840012213b93c0a9facf8a5666f2265
Kenneth Boyd <zaimoni@zaimoni.com>
2017-07-31 02:55:40 -0500

    rethink the denormalized probability class

--- RogueSurvivor/Zaimoni/Data/DenormalizedProbability.cs
+++ RogueSurvivor/Zaimoni/Data/DenormalizedProbability.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics.Contracts;
 
 namespace Zaimoni.Data
 {
@@ -53,81 +54,84 @@ namespace Zaimoni.Data
                 return _weights[x];
             }
             set {
-                if (0 < value) {
-                    _weights[x] = value;
-                    return;
-                }
-                if (_weights.ContainsKey(x)) _weights.Remove(x);
+                if (0 < value) _weights[x] = value;
+                else if (_weights.ContainsKey(x)) _weights.Remove(x);
             }
         }
 
         public void Normalize(float target = 1f)
         {
-#if DEBUG
-            if (0f >= target || 1f < target) throw new ArgumentOutOfRangeException("value to normalize to must be in (0,1]");
-#endif
+            Contract.Requires(0f < target && 1f >= target);
             if (0 >= _weights.Count) return;    // must have at least one entry to normalize
-retry:      List<T> tmp = new List<T>(_weights.Keys);
-            if (1 == tmp.Count) {   // if only one entry, trivial normalization
-                _weights[tmp[0]] = target;
+retry:      if (1 == _weights.Count) {
+              foreach(T x in _weights.Keys) {
+                _weights[x] = target;
                 return;
+              }
             }
-            List<float> tmp2 = new List<float>(_weights.Values);
-            tmp2.Sort();    // nonstrictly increasing order is generally the most numerically stable way to sum positive floating point numerals
             double sum = 0.0;
+            float scale = 1f;
+            {   // scoping brace
+                List<float> tmp2 = new List<float>(_weights.Values);
+            tmp2.Sort();    // nonstrictly increasing order is not a bad way to sum positive floating point numerals
             foreach(float tmp3 in tmp2) {
-                if (float.MaxValue - sum <= tmp3) {
-                    _divide_by_2();
+                while(float.MaxValue - sum <= tmp3 * scale) {
+                    if (_divide_by(2f)) goto retry;
+                    sum /= 2f;
+                    scale /= 2f;
+                }
+                sum += tmp3*scale;
+            }
+            }   // end scoping brace
+            // could start micro-optimizing here
+            double threshold = 2.0 * target;
+            scale = 1f;
+            float discard = float.Epsilon;
+            while(threshold <= sum/scale) {
+                scale *= 2f;
+                discard *= 2f;
+                if (_weights.Values.Any(x => x<discard)) {
+                    _divide_by(scale);
                     goto retry;
                 }
-                sum += tmp3;
             }
-            // could start micro-optimizing here
-            double target_x_2 = 2.0 * target;
-            while(target_x_2 <= sum) {
-                _divide_by_2();
-                sum /= 2.0;
-                if (1 >= _weights.Count) goto retry;    // trivialized
-            }
-            double target_div_2 = target / 2.0;
-            while(target_div_2 >= sum) {
-                _multiply_by_2();
-                sum *= 2.0;
-            }
-            double scale = target / sum;
+            threshold = target / 2f;
+            scale = 1f;
+            while(threshold >= sum*scale) scale *= 2f;
+            sum *= scale;
+            _multiply_by(scale);
+
+            scale = (float)(target / sum);
             if (1.0 == scale) return;   // already normalized
-            // we should not be capable of overflow at this point
-            if (1.0 > scale) {
-                double lb = float.Epsilon / scale;
-                bool pruned = false;
-                foreach (T tmp3 in new List<T>(_weights.Keys)) {
-                    if (lb <= _weights[tmp3]) continue;
-                    _weights.Remove(tmp3);  // would underflow
-                    pruned = true;
+            _multiply_by(scale);
+        }
+
+        // powers of 2 are the safest choice
+        private bool _divide_by(float n)
+        {
+            bool deleted = false;
+            Dictionary<T, float> tmp = new Dictionary<T, float>();
+
+            foreach (KeyValuePair<T, float> x in _weights) {
+                float tmp2 = x.Value / n;
+                if (0f >= tmp2) {
+                    deleted = true;
+                    continue;  // drop underflow
                 }
-                if (pruned) goto retry;
+                tmp[x.Key] = tmp2;
             }
-            foreach (T tmp3 in new List<T>(_weights.Keys)) {
-                _weights[tmp3] = (float)(_weights[tmp3]*scale);
-            }
+
+            _weights = tmp;
+            return deleted;
         }
 
-        // Assume we are on typical hardware: i.e. base 2 floating point numerals.
-        // then division by 2 is exact and a relatively safe way to prevent overflow.
-        private void _divide_by_2()
+        private void _multiply_by(float n)
         {
-            foreach(T tmp in new List<T>(_weights.Keys)) {
-                _weights[tmp] /= 2f;
-                if (0f >= _weights[tmp]) _weights.Remove(tmp);
-            }
-        }
+            Dictionary<T, float> tmp = new Dictionary<T, float>();
 
-        // multiplication by 2 also relatively safe on typical hardware
-        private void _multiply_by_2()
-        {
-            foreach (T tmp in new List<T>(_weights.Keys)) {
-                _weights[tmp] *= 2f;
-            }
+            foreach (KeyValuePair<T,float> x in _weights) tmp[x.Key] = 2f*x.Value;
+
+            _weights = tmp;
         }
     }
 }
