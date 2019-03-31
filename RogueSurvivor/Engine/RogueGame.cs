7d022c9e64c63273f466326d6208d5f5cd4bec5e
Kenneth Boyd <zaimoni@zaimoni.com>
2019-03-31 17:18:35 -0500

    smoke test: up/down stair loop is not really a free move

--- RogueSurvivor/Engine/RogueGame.cs
+++ RogueSurvivor/Engine/RogueGame.cs
@@ -7733,7 +7733,7 @@ namespace djack.RogueSurvivor.Engine
         }
       }
       bool run_was_free_move = actor.IsRunning && actor.RunIsFreeMove;  // we cannot quite simulate this correctly at district boundaries: the departure district will not respect the free move
-      bool need_stamina_regen = actor.IsRunning ? !actor.RunIsFreeMove : !actor.WalkIsFreeMove;
+      bool need_stamina_regen = (actor.IsRunning ? !actor.RunIsFreeMove : !actor.WalkIsFreeMove) && null!=exitAt.Location.Map.NextActorToAct;
       actor.SpendActionPoints(actor.IsRunning ? Rules.BASE_ACTION_COST/2 : Rules.BASE_ACTION_COST);
       if (actor.IsRunning) actor.SpendStaminaPoints(Rules.STAMINA_COST_RUNNING);
       MapObject mapObjectAt = exitAt.Location.Map.GetMapObjectAt(exitAt.Location.Position);
