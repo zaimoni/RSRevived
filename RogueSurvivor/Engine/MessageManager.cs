6e1cc2cd4b3c953b08d192cc48d4f988c44c3470
zaimoni <tharichizi@gmail.com>
2020-01-02 15:57:17 -0600

    C# 8.0 nullable conversion for MessageManager

--- RogueSurvivor/Engine/MessageManager.cs
+++ RogueSurvivor/Engine/MessageManager.cs
@@ -8,7 +8,8 @@ using djack.RogueSurvivor.Data;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Diagnostics.Contracts;
+
+#nullable enable
 
 namespace djack.RogueSurvivor.Engine
 {
@@ -21,7 +22,6 @@ namespace djack.RogueSurvivor.Engine
     private readonly int m_HistorySize;
     private readonly int m_DisplaySize;
 
-    public int Count { get { return m_Messages.Count; } }
     public IEnumerable<Message> History { get { return m_History; } }
 
     public MessageManager(int linesSpacing, int fadeoutFactor, int historySize, int displaySize)
@@ -39,15 +39,8 @@ namespace djack.RogueSurvivor.Engine
       m_Messages = new List<Message>(displaySize);
     }
 
-    public void Clear()
-    {
-      m_Messages.Clear();
-    }
-
-    public void ClearHistory()
-    {
-      m_History.Clear();
-    }
+    public void Clear() { m_Messages.Clear(); }
+    public void ClearHistory() { m_History.Clear(); }
 
     public void Add(Message msg)
     {
@@ -59,22 +52,17 @@ namespace djack.RogueSurvivor.Engine
 
     public void RemoveLastMessage()
     {
-      if (m_Messages.Count == 0) return;
-      m_Messages.RemoveAt(m_Messages.Count - 1);
+      int count;
+      if (0 < (count = m_Messages.Count)) m_Messages.RemoveAt(count - 1);
     }
 
-    public void Draw(IRogueUI ui, int freshMessagesTurn, int gx, int gy)
-    {
-      for (int index = 0; index < m_Messages.Count; ++index)
-      {
+    public void Draw(IRogueUI ui, int freshMessagesTurn, int gx, int gy) {
+      for (int index = 0; index < m_Messages.Count; ++index) {
         Message message = m_Messages[index];
         int alpha = Math.Max(64, (int) byte.MaxValue - m_FadeoutFactor * (m_Messages.Count - 1 - index));
-        bool flag = m_Messages[index].Turn >= freshMessagesTurn;
         Color color = Color.FromArgb(alpha, message.Color);
-        if (flag)
-          ui.UI_DrawStringBold(color, message.Text, gx, gy, new Color?());
-        else
-          ui.UI_DrawString(color, message.Text, gx, gy, new Color?());
+        if (message.Turn >= freshMessagesTurn) ui.UI_DrawStringBold(color, message.Text, gx, gy, new Color?());
+        else ui.UI_DrawString(color, message.Text, gx, gy, new Color?());
         gy += m_LinesSpacing;
       }
     }
