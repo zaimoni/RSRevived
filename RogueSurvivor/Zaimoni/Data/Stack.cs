76775d9a1d8d5c74e9d2c3088851ab93a0d9fa8a
zaimoni <tharichizi@gmail.com>
2020-02-07 12:30:46 -0600

    unbreak build: Zaimoni.Data.Stack type

---
+++ RogueSurvivor/Zaimoni/Data/Stack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zaimoni.Data
+{
+    public ref struct Stack<T>
+    {
+        private Span<T> _x;
+        private int ub;
+
+        public Stack(Span<T> src) { _x = src; ub = 0; }
+        public void push(ref T src) { _x[ub++] = src;  }
+        public void push(T src) { _x[ub++] = src;  }
+        public int Count { get { return ub; } }
+        public void Clear() { ub = 0; }
+        public T this[int n] { get { return _x[n]; } }
+    }
+}
