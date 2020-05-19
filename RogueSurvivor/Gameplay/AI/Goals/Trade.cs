f6149cd0f86ddfa984b1c5ef7819e93b7e59041c
zaimoni <tharichizi@gmail.com>
2020-05-19 13:53:50 -0500

    backpatch following activity for Trade goal

--- RogueSurvivor/Gameplay/AI/Goals/Trade.cs
+++ RogueSurvivor/Gameplay/AI/Goals/Trade.cs
@@ -92,20 +92,20 @@ namespace djack.RogueSurvivor.Gameplay.AI.Goals
                 ret = new ActionTrade(m_Actor, actor);
                 return true;
             }
-            /*
-             * Legacy code was doing:
-                    m_Actor.Activity = Activity.FOLLOWING;
-                    m_Actor.TargetActor = near.Value;
-             *  */
 
             var find_us = _whom.Select(act => act.Location);
-            var act = oai.BehaviorHeadFor(find_us.Where(loc => oai.CanSee(loc)), false, false);
+            var find_visible = find_us.Where(loc => oai.CanSee(loc));
+            var act = oai.BehaviorHeadFor(find_visible, false, false);
             if (null != act) {
+                m_Actor.Activity = Activity.FOLLOWING;
+                m_Actor.TargetActor = Rules.Get.DiceRoller.Choose(find_visible).Actor;   // XXX possibly misleading
                 ret = act;
                 return true;
             }
             act = oai.BehaviorPathTo(new HashSet<Location>(find_us));
             if (null != act) {
+                m_Actor.Activity = Activity.FOLLOWING;
+                m_Actor.TargetActor = Rules.Get.DiceRoller.Choose(_whom);   // XXX possibly misleading
                 ret = act;
                 return true;
             }
