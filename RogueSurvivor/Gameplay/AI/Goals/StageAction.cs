a4fef8169ef1ccdc36cd110ad9ae6e4750100fc0
zaimoni <tharichizi@gmail.com>
2020-09-02 09:40:50 -0500

    unbreak build

---
+++ RogueSurvivor/Gameplay/AI/Goals/StageAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using djack.RogueSurvivor.Data;
+
+namespace djack.RogueSurvivor.Gameplay.AI.Goals
+{
+    [Serializable]
+    internal class StageAction : Objective
+    {
+      public readonly ActorAction Intent;
+
+      public StageAction(int t0, Actor who, ActorAction free_act) : base(t0,who)
+      {
+#if DEBUG
+        if (!(who.Controller is ObjectiveAI)) throw new ArgumentOutOfRangeException(nameof(who), who, "not intelligent enough to plan ahead");
+#endif
+        Intent = free_act;
+      }
+
+      // always execute.  Expire on execution
+      public override bool UrgentAction(out ActorAction ret)
+      {
+        ret = null;
+        if (Intent.IsPerformable() && !Intent.Abort()) {
+          // XXX need some sense of what a combat action is
+          var ai = m_Actor.Controller;
+          if (null != ai.enemies_in_FOV) return false;
+
+          var oai = (ai as ObjectiveAI)!;
+          if (!oai.VetoAction(Intent)) oai.Stage(Intent);
+        }
+       _isExpired = true;
+        return true;
+      }
+    }
+}
---
+++ RogueSurvivor/Zaimoni/Data/Operator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+#nullable enable
+
+namespace Zaimoni.Data
+{
+    public interface Operator<in SRC,out OP> where OP:class
+    {
+        bool IsLegal();
+        OP? Bind(SRC src);
+    }
+
+    internal interface BackwardPlan<OP>
+    {
+      List<OP>? prequel();
+      Dictionary<OP,int>? backward();
+    }
+}
