08a9794b7839541515a6fabe857b20279f7420c7
zaimoni <tharichizi@gmail.com>
2020-09-17 14:48:08 -0500

    checkpoint: observer pattern support

---
+++ RogueSurvivor/Zaimoni/Data/Observer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zaimoni.Data
+{
+    // observer pattern support
+    interface Observer<in T>
+    {
+        /// <returns>true if and only if should be de-registered</returns>
+        bool update(T src);
+    }
+
+    [Serializable]
+    public class Observed<T>
+    {
+        private List<Observer<T>> m_Watchers;
+
+        void update(T src) {
+            var ub = m_Watchers.Count;
+            while (0 <= --ub) {
+                if (m_Watchers[ub].update(src)) m_Watchers.RemoveAt(ub);
+            }
+        }
+
+        void Add(Observer<T> src) { m_Watchers.Add(src); }
+    }
+}
