c37e96dd6af4bc076eb1ea5d642a8aa94220fbca
zaimoni <tharichizi@gmail.com>
2020-11-12 17:11:32 -0600

    modernize assembly info configuration

--- RogueSurvivor/AssemblyInfo.cs
+++ RogueSurvivor/AssemblyInfo.cs
@@ -4,15 +4,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using djack.RogueSurvivor;
 
-[assembly: AssemblyCompany("Zaimoni.com")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2012 (roguedjack),2016")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8a2b9c0a-6dcb-4dfc-b96a-ade733831802")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
-[assembly: AssemblyConfiguration("")]
-[assembly: NeutralResourcesLanguage("en")]
 [assembly: AssemblyTitle(SetupConfig.GAME_NAME+" " + SetupConfig.GAME_VERSION)]
-[assembly: AssemblyVersion("0.9.1.0")]
