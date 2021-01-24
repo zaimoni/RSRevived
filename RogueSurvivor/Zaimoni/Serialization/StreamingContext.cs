4aadb940d9fd7b5df8bd58896122e19e6d390fb5
zaimoni <tharichizi@gmail.com>
2021-01-24 09:57:28 -0600

    checkpoint cloning System.Runtime.Serialization

---
+++ RogueSurvivor/Zaimoni/Serialization/StreamingContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+
+namespace Zaimoni.Serialization
+{
+    /// <summary>
+    /// Part of cloning the System.Runtime.Serialization specification, which is deprecated and cannot be assumed to be available indefinitely.
+    /// </summary>
+    public readonly struct StreamingContext
+    {
+        public readonly States State;
+        public readonly object? Context;
+
+        StreamingContext(States st, object? xtra = null) {
+            State = st;
+            Context = xtra;
+        }
+
+        // comment out the ones we don't handle
+        [Flags]
+        public enum States : byte {
+//          CrossProcess = 1, // different process on same computer
+//          CrossMachine = 2, // different computer
+            File = 4, // files can last longer than the process, and do not require data from the process to deserialize
+//          Persistence = 8, // some sort of persisted store; can last longer than the process, and do not require data from the process to deserialize
+//          Remoting = 16, // context in an unknown location; may or may not be on same computer
+//          Other = 32, // explicitly unknown (not foreseen)
+//          Clone = 64, // in-memory deep clone within same process; handles/references to unmanaged resources should remain valid
+//          CrossAppDomain = 128, // different Application Domain
+            All = 255 // any defined source/destination context; morally unknown
+        }
+    }
+}
