46fb9a99c142f6056ce8c269eb80df8a7850d83a
zaimoni <tharichizi@gmail.com>
2021-03-20 02:26:59 -0500

    checkpoint DecodeObjects class

---
+++ RogueSurvivor/Zaimoni/Serialization/DecodeObjects.cs
@@ -0,0 +1,51 @@
+﻿#define FORCE_STD_DICTIONARY_GC
+
+using System;
+using System.Collections.Generic;
+
+#nullable enable
+
+namespace Zaimoni.Serialization
+{
+    class DecodeObjects
+    {
+        private Dictionary<ulong, List<Action<object>>?> requested = new();
+        private Dictionary<Type, Dictionary<ulong, object>> encodings = new();
+
+        public object Seen(ulong id)
+        {
+            if (requested.ContainsKey(id)) return null; // already seen, not yet recorded
+            foreach (var x in encodings.Values) {
+                if (x.TryGetValue(id, out var ret)) return ret;
+            }
+            requested.Add(id, null); // record that we want this
+            return null;
+        }
+
+        // Intended model is a handler that assigns an object, then starts a no-op task to signal that the assignment has happened.
+        public void Schedule(ulong id, Action<object> handler)
+        {
+            if (!requested.TryGetValue(id, out var exec)) throw new InvalidOperationException("can only schedule handlers for a registered object");
+            if (null == exec) requested[id] = (exec = new());
+            exec.Add(handler);
+        }
+
+        public void Register<T>(ulong id, T src) where T : class
+        {
+            if (null == src) throw new ArgumentNullException(nameof(src));
+            if (!requested.TryGetValue(id, out var exec)) throw new InvalidOperationException("only can register a constructed object once");
+            // exec is a list of object handlers "waiting" on this registration
+            var type = typeof(T);
+            if (encodings.TryGetValue(type, out var cache)) {
+                if (cache.ContainsKey(id)) throw new InvalidOperationException("only can register a constructed object once");
+            } else encodings.Add(type, (cache = new()));
+            cache.Add(id, src);
+#if FORCE_STD_DICTIONARY_GC
+            requested[id] = null;
+#endif
+            requested.Remove(id);
+            if (null != exec) foreach (var handler in exec) handler(src);
+        }
+
+    }
+}
--- RogueSurvivor/Zaimoni/Serialization/EncodeObjects.cs
+++ RogueSurvivor/Zaimoni/Serialization/EncodeObjects.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Zaimoni.Serialization
 {
@@ -7,6 +8,7 @@ namespace Zaimoni.Serialization
     {
         private ulong seen = 0;
         private Dictionary<Type, Dictionary<object, ulong>> encodings = new();
+        private List<KeyValuePair<ulong, Action<Stream>>> to_save = new();
 
         public ulong Saving<T>(T src) where T:class
         {
@@ -14,10 +16,20 @@ namespace Zaimoni.Serialization
             if (encodings.TryGetValue(type, out var cache)) {
                 if (cache.TryGetValue(src, out var code)) return code;
                 cache.Add(src, ++seen);
-                return seen;
+            } else {
+                encodings.Add(type, new Dictionary<object, ulong> { [src] = ++seen });
             }
-            encodings.Add(type, new Dictionary<object, ulong> { [src] = ++seen });
+            to_save.Add(new(seen, null)); // \todo construct action for saving src and schedule it
             return seen;
+        }
+
+        public bool SaveNext(Stream dest) {
+            if (0 >= to_save.Count) return false;
+            var next = to_save[0];
+            to_save.RemoveAt(0);
+            // \todo write object id at next.Key to hard drive
+            // \todo write object itself to hard drive
+            return true;
         }
     }
 }
