cbaa70fe077363062b3b5e79f9c46bd0164ceec3
zaimoni <tharichizi@gmail.com>
2021-04-21 05:31:43 -0500

    checkpoint System.Runtime.Serialization replacement

---
+++ RogueSurvivor/Zaimoni/Serialization/Formatter.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaimoni.Serialization
+{
+    abstract public class Formatter
+    {
+        protected readonly StreamingContext _context;
+
+        public Formatter(StreamingContext context) {
+            _context = context;
+        }
+
+        // need to work out how to handle struct and enum (defective C# generics compared to C++)
+        public T Deserialize<T>(Stream src) where T:class
+        {
+            if (!src.CanRead) throw new InvalidOperationException(nameof(src)+" cannot read");
+            var anchor = new LoadManager(_context);
+            return Deserialize<T>(src, anchor);
+        }
+
+        public void Serialize<T>(Stream dest, T src) where T:class
+        {
+            if (!dest.CanWrite) throw new InvalidOperationException(nameof(src) + " cannot write");
+            var anchor = new SaveManager(_context);
+            Serialize(dest, src, anchor);
+        }
+
+        private T Deserialize<T>(Stream src, LoadManager anchor) where T:class
+        {
+            var expected = typeof(T);
+            // if we have a constructor from formatter, call it
+            // if we have a static override, use it
+
+            // obsolete:
+            // read the type spec from the stream
+            // then pull in the top-level type if we recognize it
+            return default;
+        }
+
+        private void Serialize<T>(Stream dest, T src, SaveManager anchor) where T:class
+        {
+            var expected = typeof(T);
+            // if we have a direct-saver, use it
+
+            // obsolete:
+            // ensure the type T is encoded in the stream
+            // then
+        }
+
+        private void Deserialize<T>(Stream dest, ref T src, LoadManager anchor) where T : class
+        {
+            // ensure the type T is encoded in the stream
+            // then
+        }
+
+#if FAIL
+        // this strategy doesn't actually work in C#: we don't have the means to
+        // call a non-generic function from a generic function efficiently.
+        // (System.Reflection is not efficient)
+        protected abstract bool trivialSerialize<T>(Stream dest, T src);
+        protected abstract bool trivialDeserialize<T>(Stream dest, ref T src);
+#endif
+        // structs have to go through code generation using these as exemplars
+#region integer basis
+        public abstract void Serialize(Stream dest, ulong src);
+        public abstract void Serialize(Stream dest, long src);
+        public abstract void Deserialize(Stream src, ref ulong dest);
+        public abstract void Deserialize(Stream src, ref long dest);
+#endregion
+
+#region integer adapters
+        public void Serialize(Stream dest, uint src) { Serialize(dest, (ulong)src); }
+        public void Serialize(Stream dest, ushort src) { Serialize(dest, (ulong)src); }
+
+        public void Serialize(Stream dest, int src) { Serialize(dest, (long)src); }
+        public void Serialize(Stream dest, short src) { Serialize(dest, (long)src); }
+
+        public void Deserialize(Stream src, ref uint dest) {
+            ulong relay = 0;
+            Deserialize(src, ref relay);
+            if (uint.MaxValue < relay) throw new InvalidDataException("huge uint found");
+            dest = (uint)relay;
+        }
+
+        public void Deserialize(Stream src, ref ushort dest) {
+            ulong relay = 0;
+            Deserialize(src, ref relay);
+            if (ushort.MaxValue < relay) throw new InvalidDataException("huge ushort found");
+            dest = (ushort)relay;
+        }
+
+        public void Deserialize(Stream src, ref int dest)
+        {
+            long relay = 0;
+            Deserialize(src, ref relay);
+            if (int.MaxValue < relay || int.MinValue > relay) throw new InvalidDataException("huge int found");
+            dest = (int)relay;
+        }
+
+        public void Deserialize(Stream src, ref short dest)
+        {
+            long relay = 0;
+            Deserialize(src, ref relay);
+            if (short.MaxValue < relay || short.MinValue > relay) throw new InvalidDataException("huge short found");
+            dest = (short)relay;
+        }
+#endregion
+
+#region byte basis
+        public void Serialize(Stream dest, byte src) { trivialSerialize(dest, src); }
+        public void Serialize(Stream dest, sbyte src) { trivialSerialize(dest, src); }
+        public void Serialize(Stream dest, char src) { trivialSerialize(dest, src); }
+        public void Deserialize(Stream src, ref byte dest) { trivialDeserialize(src, ref dest); }
+        public void Deserialize(Stream src, ref sbyte dest) { trivialDeserialize(src, ref dest); }
+        public void Deserialize(Stream src, ref char dest) { trivialDeserialize(src, ref dest); }
+
+        // outside users should not need to provide additional overrides for the trivialSerialize/trivialDeserialize naming scheme
+        protected abstract void trivialSerialize(Stream dest, byte src);
+        protected abstract void trivialDeserialize(Stream src, ref byte dest);
+        protected abstract void trivialSerialize(Stream dest, char src);
+        protected abstract void trivialDeserialize(Stream src, ref char dest);
+        protected abstract void trivialSerialize(Stream dest, sbyte src);
+        protected abstract void trivialDeserialize(Stream src, ref sbyte dest);
+#endregion
+    }
+
+    public class BinaryFormatter : Formatter
+    {
+        public BinaryFormatter(StreamingContext context) : base(context) { }
+
+#if FAIL
+        protected override bool trivialSerialize<T>(Stream dest, T src) {
+            var method = typeof(BinaryFormatter).GetMethod("_trivialSerialize", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic,
+                null, System.Reflection.CallingConventions.Standard, new Type[]{ typeof(Stream), typeof(T)}, null);
+            if (null == method) return false;
+            method.Invoke(null, new object[] { src });
+            return true;
+        }
+
+        protected override bool trivialDeserialize<T>(Stream dest, ref T src) {
+            var method = typeof(BinaryFormatter).GetMethod("_trivialDeserialize", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic,
+                null, System.Reflection.CallingConventions.Standard, new Type[]{ typeof(Stream), typeof(T).MakeByRefType() }, null);
+            if (null == method) return false;
+            T relay;
+            method.Invoke(null, new object[] { dest, relay });
+            src = relay;
+            return true;
+        }
+
+        static private void _trivialSerialize(Stream dest, byte src) { dest.WriteByte(src); }
+        static private void _trivialSerialize(Stream dest, char src) { dest.WriteByte((byte)src); }
+
+        static private void _trivialDeserialize(Stream src, ref byte dest) {
+            var code = src.ReadByte();
+            if (-1 == code) throw new InvalidOperationException("stream ended unexpectedly");
+            dest = (byte)code;
+        }
+
+        static private void _trivialDeserialize(Stream src, ref char dest)
+        {
+            var code = src.ReadByte();
+            if (-1 == code) throw new InvalidOperationException("stream ended unexpectedly");
+            dest = (char)code;
+        }
+#endif
+
+#region byte basis
+        protected override void trivialSerialize(Stream dest, byte src) { dest.WriteByte(src); }
+        protected override void trivialSerialize(Stream dest, char src) { dest.WriteByte((byte)src); }
+        protected override void trivialSerialize(Stream dest, sbyte src) { dest.WriteByte((byte)src); }
+
+        protected override void trivialDeserialize(Stream src, ref byte dest)
+        {
+            var code = src.ReadByte();
+            if (-1 == code) throw new InvalidDataException("stream ended unexpectedly");
+            dest = (byte)code;
+        }
+        protected override void trivialDeserialize(Stream src, ref char dest)
+        {
+            var code = src.ReadByte();
+            if (-1 == code) throw new InvalidDataException("stream ended unexpectedly");
+            dest = (char)code;
+        }
+        protected override void trivialDeserialize(Stream src, ref sbyte dest)
+        {
+            var code = src.ReadByte();
+            if (-1 == code) throw new InvalidDataException("stream ended unexpectedly");
+            dest = (sbyte)code;
+        }
+#endregion
+
+#region integer basis
+        private sbyte _format(ulong src, in Span<byte> dest)
+        {
+            sbyte ub = 0;
+            while (0 < src) {
+                dest[ub++] = (byte)(src % 256);
+                src /= 256;
+            }
+            return ub;
+        }
+
+        // following file:///C:/Ruby27-x64/share/doc/ruby/html/marshal_rdoc.html
+        public override void Serialize(Stream dest, ulong src) {
+            Span<byte> relay = stackalloc byte[8];
+            var ub = _format(src, in relay);
+            trivialSerialize(dest, ub);
+            if (1 <= ub) {
+                byte scan = 0;
+                do {
+                    trivialSerialize(dest, relay[scan]);
+                } while (++scan < ub);
+            }
+        }
+
+        public override void Deserialize(Stream src, ref ulong dest) {
+            dest = 0;
+            ulong scale = 1;
+            sbyte ub = 0;
+            trivialDeserialize(src, ref ub);
+            if (0 > ub || 8 < ub) throw new InvalidDataException("does not fit in ulong");
+            byte scan = 0;
+            while (0 < ub) {
+                trivialDeserialize(src, ref scan);
+                dest += scale * scan;
+                scale *= 256;
+                --ub;
+            }
+        }
+
+        // we, like Ruby, are relying on the hardware signed integer format being 2's-complement
+        // that is, "trailing" 0xFF are not significant
+        // the two other C-supported representations for negative integers require different handling
+        public override void Serialize(Stream dest, long src)
+        {
+            if (0 <= src) {
+                Serialize(dest, (ulong)src);
+                return;
+            }
+
+            Span<byte> relay = stackalloc byte[8];
+            var ub = _format((ulong)src, in relay);
+            while (2 <= ub && 255 == relay[ub - 1]) --ub;
+            trivialSerialize(dest, (sbyte)(-ub));
+            if (1 <= ub) {
+                byte scan = 0;
+                do {
+                    trivialSerialize(dest, relay[scan]);
+                } while (++scan < ub);
+            }
+        }
+
+        public override void Deserialize(Stream src, ref long dest) {
+            dest = 0;
+            long scale = 1;
+            sbyte ub = 0;
+            trivialDeserialize(src, ref ub);
+            if (-8 > ub || 8 < ub) throw new InvalidDataException("does not fit in long");
+            byte scan = 0;
+            while (0 < ub) {
+                trivialDeserialize(src, ref scan);
+                dest += scale * scan;
+                scale *= 256;
+                --ub;
+            }
+            if (0 <= ub) return;
+            while (0 > ub) {
+                trivialDeserialize(src, ref scan);
+                var test = scan - 256;
+                dest += scale * scan;
+                scale *= 256;
+                ++ub;
+            }
+        }
+#endregion
+    }
+}
---
+++ RogueSurvivor/Zaimoni/Serialization/LoadManager.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using Zaimoni.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+
+namespace Zaimoni.Serialization
+{
+    interface IOnDeserializing // this may not make much sense in a proper RAII context
+    {
+        void OnDeserializing(in StreamingContext context);
+    }
+
+    interface IOnDeserialized
+    {
+        void OnDeserialized();
+    }
+
+    interface IOnFullyDeserialized
+    {
+        void OnFullyDeserialized();
+    }
+
+
+    /// <summary>
+    /// Part of cloning the System.Runtime.Serialization specification, which is deprecated and cannot be assumed to be available indefinitely.
+    /// This corresponds to ObjectManager
+    /// </summary>
+    public sealed class LoadManager
+    {
+        private readonly StreamingContext _context;
+        private readonly Dictionary<int, List<Action<object>>> _fixups = new Dictionary<int, List<Action<object>>>();
+        private readonly List<KVpair<int, KeyValuePair<object, Type>?>> _seen = new List<KVpair<int, KeyValuePair<object, Type>?>>(); // would prefer a dictionary-type data structure here
+        private readonly List<IOnFullyDeserialized?> _onDeserializedTargets = new List<IOnFullyDeserialized?>(); // could use event syntax for this
+
+        public LoadManager(StreamingContext context)
+        {
+            _context = context;
+        }
+
+        public object? GetObject(int id) {
+            if (0 >= id) throw new ArgumentOutOfRangeException(nameof(id), "0 >= "+id.ToString());
+            lock (_seen) {
+                // not what we want
+                var ub = _seen.Count;
+                while (0 <= --ub) {
+                    if (_seen[ub].Key == id) {
+                        if (null == _seen[ub].Value) return null; // forward-reference
+                        return _seen[ub].Value.Value.Key;
+                    }
+                }
+                _seen.Add(new KVpair<int, KeyValuePair<object, Type>?>(id, null)); // assume that it will be there eventually
+            }
+            return null;
+        }
+
+        private void exec_fixup(int id, object src)
+        {
+            lock (_fixups) {
+                if (_fixups.TryGetValue(id, out var cache)) {
+                    foreach (var op in cache) op(src);
+                    _fixups.Remove(id);
+                }
+            }
+        }
+
+        static private void RaiseOnDeserializedEvent<T>(T obj) {
+            if (obj is IOnDeserialized x) x.OnDeserialized();
+        }
+
+        public void RaiseOnDeserializedEvent()
+        { // \todo remove if redundant
+            lock (_onDeserializedTargets) {
+                var ub = _onDeserializedTargets.Count;
+                // \todo what we would like to do is "move" the backing array without copying,
+                // do the countdown on the backing array, then let it scope out
+                while (0 < --ub) {
+                    _onDeserializedTargets[ub]!.OnFullyDeserialized();
+                    _onDeserializedTargets[ub] = null; // trigger GC if we had boxed a struct
+                }
+                _onDeserializedTargets.Clear();
+            }
+        }
+
+        private void RegisterFullDeserialization<T>(T obj) {
+            if (obj is IOnFullyDeserialized x) lock (_onDeserializedTargets) { _onDeserializedTargets.Add(x); }
+        }
+
+        public void Register<T>(T obj, int id) where T:class {
+            if (null == obj) throw new ArgumentNullException(nameof(obj));
+            if (0 >= id) throw new ArgumentOutOfRangeException(nameof(id), "0 >= " + id.ToString());
+            var src = new KeyValuePair<object, Type>(obj, obj.GetType());
+            lock (_seen) {
+                var ub = _seen.Count;
+                while (0 <= --ub) {
+                    if (_seen[ub].Key == id) { // \todo separate exception type for serialization errors
+                        if (null != _seen[ub].Value) throw new InvalidOperationException("already registered "+id.ToString());
+                        _seen[ub].Value = src;
+                        exec_fixup(id, obj);
+                        RaiseOnDeserializedEvent(obj);
+                        RegisterFullDeserialization(obj);
+                        return;
+                    }
+                }
+                _seen.Add(new KVpair<int, KeyValuePair<object, Type>?>(id, src));
+                exec_fixup(id, obj);
+                RaiseOnDeserializedEvent(obj);
+                RegisterFullDeserialization(obj);
+            }
+        }
+
+        public void RegisterFixup(int id, Action<object> handler) {
+            lock (_fixups) {
+                if (_fixups.TryGetValue(id, out var cache)) cache.Add(handler);
+                else _fixups.Add(id, new List<Action<object>> { handler });
+            }
+        }
+
+        public void RaiseOnSerializingEvent<T>(T obj) {
+            if (obj is IOnDeserializing x) x.OnDeserializing(in _context);
+        }
+    }
+}
