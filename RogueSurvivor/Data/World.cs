14576e8fd612278abaf9653c976f4e7a37e6a8e1
zaimoni <tharichizi@gmail.com>
2021-11-26 15:16:27 -0600

    diagonal tile fixup

--- RogueSurvivor/Data/World.cs
+++ RogueSurvivor/Data/World.cs
@@ -4,7 +4,7 @@
 // MVID: D2AE4FAE-2CA8-43FF-8F2F-59C173341976
 // Assembly location: C:\Private.app\RS9Alpha.Hg\RogueSurvivor.exe
 
-// #define BOOTSTRAP_HIGHWAY
+#define BOOTSTRAP_HIGHWAY
 
 using System;
 using System.Collections.Generic;
