0e59dcd4fb4ff916544f788cafd9264be6e632f3
zaimoni <tharichizi@gmail.com>
2022-05-12 05:28:16 -0500

    checkpoint WorldUpdate-based Chain class

---
+++ RogueSurvivor/Engine/Action/Chain.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using djack.RogueSurvivor.Data;
+using Zaimoni.Data;
+
+using ObjectiveAI = djack.RogueSurvivor.Gameplay.AI.ObjectiveAI;
+using ActionChain = djack.RogueSurvivor.Engine.Actions.ActionChain;
+
+namespace djack.RogueSurvivor.Engine.Op
+{
+    [Serializable]
+    internal class Chain : WorldUpdate
+    {
+        private readonly List<WorldUpdate> m_Actions;
+
+        // for now, assume any constructor ActionChain needs, we need an analog for
+        public Chain(List<WorldUpdate> actions)
+        {
+#if DEBUG
+            if (null == actions || 2 > actions.Count) throw new ArgumentNullException(nameof(actions));
+            if (actions.Count > _forkBefore(actions)) throw new ArgumentOutOfRangeException(nameof(actions), actions, "fork may only terminate an action chain");
+#endif
+            m_Actions = actions;
+        }
+
+        public Chain(WorldUpdate act0, WorldUpdate act1)
+        {
+            var chain1 = act1 as Chain;
+#if DEBUG
+            if (null != chain1 && chain1.m_Actions.Count > _forkBefore(chain1.m_Actions)) throw new ArgumentOutOfRangeException(nameof(chain1), chain1, "fork may only terminate an action chain");
+#endif
+            if (act0 is Chain chain0) {
+#if DEBUG
+                if (chain0.m_Actions.Count >= _forkBefore(chain0.m_Actions)) throw new ArgumentOutOfRangeException(nameof(chain0), chain0, "fork may only terminate an action chain");
+#endif
+                m_Actions = new(chain0.m_Actions);
+                if (null != chain1) m_Actions.AddRange(chain1.m_Actions);
+                else m_Actions.Add(act1);
+            } else if (null != chain1) {
+                m_Actions = new(chain1.m_Actions);
+                m_Actions.Insert(0, act0);
+            } else {
+                m_Actions = new(){ act0, act1 };
+            }
+        }
+
+        public Chain(List<WorldUpdate> src, int lb)
+        {
+#if DEBUG
+            if (null == src || src.Count <= lb) throw new ArgumentNullException(nameof(src));
+#endif
+            var actions = new List<WorldUpdate>();
+            while (lb < src.Count) actions.Add(src[lb++]);
+#if DEBUG
+            if (actions.Count > _forkBefore(actions)) throw new ArgumentOutOfRangeException(nameof(actions), actions, "fork may only terminate an action chain");
+#endif
+            m_Actions = actions;
+        }
+
+        public override bool IsLegal() {
+            foreach (var act in m_Actions) if (!act.IsLegal()) return false;
+            return true;
+        }
+
+        /// <returns>null, or a Performable action</returns>
+        public override ActorAction? Bind(Actor src) {
+#if DEBUG
+            if (!(src.Controller is ObjectiveAI)) throw new InvalidOperationException("controller not smart enough to plan actions");
+#endif
+            // unclear what is correct here.  Be conservative.
+            return m_Actions[0].Bind(src);
+        }
+
+        public override bool IsRelevant() { return false; }
+        public override bool IsRelevant(Location loc) { return m_Actions[0].IsRelevant(loc); }
+        public override bool IsSuppressed(Actor a) { return m_Actions[0].IsSuppressed(a); } // return to this later
+        public override void Blacklist(HashSet<Location> goals) { m_Actions[0].Blacklist(goals); }
+        public override void Goals(HashSet<Location> goals) { m_Actions[0].Goals(goals); }
+
+
+        private static int _forkBefore(List<WorldUpdate> src)
+        {
+            int lb = 0;
+            while (src.Count > lb) if (src[lb++] is Fork) return lb;
+            return int.MaxValue;
+        }
+    }
+}
