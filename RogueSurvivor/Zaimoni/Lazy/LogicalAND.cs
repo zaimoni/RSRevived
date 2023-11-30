e44e3b8bf581cb33971d5664a2c6dd20e1e33ea8
zaimoni <tharichizi@gmail.com>
2023-11-30 07:00:22 -0600

    respecify Zaimoni.Lazy computation to work off of Kleene's strong three-valued logic

--- RogueSurvivor/Zaimoni/Lazy/LogicalAND.cs
+++ RogueSurvivor/Zaimoni/Lazy/LogicalAND.cs
@@ -1,82 +1,256 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 #nullable enable
 
 namespace Zaimoni.Lazy
 {
+    // use case: triggering cache variable update when data completely loads.
+
     public interface hook
     {
+        // Kleene's strong 3-valued logic: true, false, null
+        bool? code { get; }
         bool signal();
-        bool isExpired();
+        void update(hook h);
+    }
+
+    class Relay : hook {
+        bool? _code = null;
+        private List<hook>? exec;
+        public void AddListener(hook src) => (exec ??= new()).Add(src);
+
+        public Relay() { }
+        public bool? code { get { return _code; } }
+        public bool signal() => null != _code;
+        public void update(hook h) { }
+
+
+        public void fire() {
+            _code = true;
+            _notify();
+        }
+
+        public void fail() {
+            _code = false;
+            _notify();
+        }
+
+        private void _notify() {
+            if (null != exec) {
+                var ub = exec.Count;
+                while (0 <= --ub) {
+                    exec[ub].signal();
+                    exec[ub] = null; // enable garbage collector early
+                }
+                exec = null;
+            }
+        }
+    }
+
+    class Notice : hook
+    {
+        private bool? _code = null;
+        private Func<bool?>? op = null;
+
+        public Notice(Func<bool?>? op)
+        {
+            this.op = op;
+        }
+
+        public bool? code { get { return _code; } }
+        public void update(hook h) { }
+
+        public bool signal() {
+            if (null == _code && null != op) {
+                _code = op();
+                if (null != _code) op = null;
+            }
+            return null != _code;
+        }
+
+    }
+
+    class ExecOnDemand : hook
+    {
+        private bool? _code = null;
+        private List<hook>? exec = null;
+        public void AddListener(hook src) => (exec ??= new()).Add(src);
+
+        public ExecOnDemand() {}
+        public bool? code { get { return _code; } }
+        public void update(hook h) { }
+
+        public bool signal() {
+            if (null == exec) {
+                _code = true;
+                return true;
+            }
+
+            exec.AND_hooks(ref _code);
+            if (0 >= exec.Count) {
+                exec = null;
+                if (null == _code) {
+                    _code = true;
+                    return true;
+                }
+            }
+            return _code ?? false;
+        }
+
     }
 
     class LogicalAND : hook
     {
-        long latch = 0;
-        Func<bool> op;
-        List<hook>? notify = null;
+        private bool? _code = null;
+        private List<hook>? guard = null;
+        private List<hook>? exec = null;
+        public void AddListener(hook src) => (exec ??= new()).Add(src);
+        public void AddGuard(hook src) => (guard ??= new()).Add(src);
+
+        public LogicalAND() { }
+        public bool? code { get { return _code; } }
+        public void update(hook h) {
+            if (null == h.code) return;
+            if (null == guard) return;
+            bool test = h.code.Value;
+            var ub = guard.Count;
+            while (0 <= --ub) {
+                if (h == guard[ub]) {
+                    if (h.code.Value) {
+                        guard.RemoveAt(ub);
+                        if (0 >= guard.Count) guard = null;
+                        return;
+                    }
+                    _code = false;
+                    guard = null;
+                    exec = null;
+                    return;
+                }
+            }
+        }
 
         public bool signal() {
-            if (0 < --latch) return false;
-            if (!op()) return false;
-            if (null != notify) {
-                var ub = notify.Count;
-                while (0 <= --ub) {
-                    if (!notify[ub].isExpired()) notify[ub].signal();
-                    notify[ub] = null;
-                }
-                notify = null;
+            if (null != _code) return _code.Value;
+            if (null != guard) {
+                guard.AND_hooks(ref _code);
+                if (0 >= guard.Count) guard = null;
+                if (null != _code) return _code.Value;
             }
-            return true;
-        }
 
-        public bool isExpired() => 0 >= latch;
-        public void Targetted() => latch++;
-
-        public void AddListener(hook src) {
-            (notify ??= new()).Add(src);
-        }
-        public void RemoveListener(hook src)
-        {
-            if (null != notify && notify.Remove(src) && 0 >= notify.Count) notify = null;
+            if (null == exec) {
+                _code = true;
+                return true;
+            }
+            exec.AND_hooks(ref _code);
+            if (0 >= exec.Count) {
+                exec = null;
+                if (null == _code) {
+                    _code = true;
+                    return true;
+                }
+            }
+            return _code ?? false;
         }
     }
 
     class LogicalOR : hook
     {
-        bool _expired = false;
-        Func<bool> op;
-        List<hook>? notify = null;
+        private bool? _code = null;
+        private List<hook>? guard = null;
+        private List<hook>? exec = null;
+        public void AddListener(hook src) => (exec ??= new()).Add(src);
+        public void AddGuard(hook src) => (guard ??= new()).Add(src);
+
+        public LogicalOR() { }
+        public bool? code { get { return _code; } }
+        public void update(hook h) {
+            if (null == h.code) return;
+            if (null == guard) return;
+            bool test = h.code.Value;
+            var ub = guard.Count;
+            while (0 <= --ub) {
+                if (h == guard[ub]) {
+                    if (!h.code.Value) {
+                        guard.RemoveAt(ub);
+                        if (0 >= guard.Count) guard = null;
+                        return;
+                    }
+                    guard = null;
+                    return;
+                }
+            }
+        }
 
         public bool signal()
         {
-            if (_expired) return true;
-            if (!op()) return false;
-            _expired = true;
-            if (null != notify) {
-                var ub = notify.Count;
-                while (0 <= --ub) {
-                    if (!notify[ub].isExpired()) notify[ub].signal();
-                    notify[ub] = null;
+            if (null != _code) return true;
+            if (null != guard) {
+                bool? ret = guard.OR_hooks();
+                if (null != ret) {
+                    guard = null;
+                    if (!ret.Value) {
+                        _code = false;
+                        return true;
+                    }
                 }
-                notify = null;
             }
-            return true;
+
+            if (null == exec) {
+                _code = true;
+                return true;
+            }
+            exec.AND_hooks(ref _code);
+            if (0 >= exec.Count) {
+                exec = null;
+                if (null == _code) {
+                    _code = true;
+                    return true;
+                }
+            }
+            return _code ?? false;
+        }
+    }
+
+    static public class Lazy_ext
+    {
+        static public void AND_hooks(this List<hook> exec, ref bool? code) {
+            var ub = exec.Count;
+            while (0 <= --ub) {
+                var test = exec[ub].code;
+                if (null != test) {
+                    if (false == test.Value) code = false;
+                    exec.RemoveAt(ub);
+                    continue;
+                }
+                if (exec[ub].signal()) {
+                    var res = exec[ub].code.Value;
+                    if (!res) code = false;
+                    exec.RemoveAt(ub);
+                    continue;
+                }
+            }
         }
 
-        public bool isExpired() => _expired;
-
-        public void AddListener(hook src)
+        static public bool? OR_hooks(this List<hook> exec)
         {
-            (notify ??= new()).Add(src);
-        }
-        public void RemoveListener(hook src)
-        {
-            if (null != notify && notify.Remove(src) && 0 >= notify.Count) notify = null;
+            var ub = exec.Count;
+            while (0 <= --ub) {
+                var test = exec[ub].code;
+                if (null != test) {
+                    if (test.Value) return true;
+                    exec.RemoveAt(ub);
+                    continue;
+                }
+                if (exec[ub].signal()) {
+                    var res = exec[ub].code.Value;
+                    if (res) return true;
+                    exec.RemoveAt(ub);
+                    continue;
+                }
+            }
+            return (0 >= exec.Count) ? false : null;
         }
     }
 }
